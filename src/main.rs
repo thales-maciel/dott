60eb05bd688468fcd2bd204e0108c5ff8fb7404d
Thales Maciel <thales@thalesmaciel.com>
2023-06-16 15:55:30 -0300

    one more refactor

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-#![allow(unused)]
-
 use crate::prelude::*;
 
 mod error;
@@ -9,95 +7,124 @@ use clap::{Args, Parser, Subcommand};
 use directories::{BaseDirs, ProjectDirs};
 use git2::Repository;
 use glob::{glob, Pattern};
-use itertools::Itertools;
 use std::{
     env,
-    fs::{copy, remove_file, create_dir_all, read_to_string},
-    path::PathBuf,
+    fs::{create_dir_all, read_to_string, remove_file, copy},
+    path::PathBuf, io,
 };
 
-
-pub trait SyncedDir {
-    fn get_file_paths(&self) -> Result<Vec<PathBuf>>;
-    fn update_files(&self, source: &Slice) -> Result<()>;
-}
-
-
-pub struct Slice {
+pub struct Dir {
     root_path: PathBuf,
-    globs: Vec<String>,
-    ignore_patterns: Vec<String>,
+    tracked_files: Vec<TrackedFile>,
 }
 
-fn is_ignored(ignore_patterns: &Vec<String>, path: &PathBuf, root_path: &PathBuf) -> bool {
-    ignore_patterns
-        .into_iter()
-        .map(|g| Pattern::new(root_path.join(g).to_str().unwrap()).unwrap())
-        .any(|p| p.matches(&path.to_str().unwrap()))
+struct TrackedFile {
+    absolute_path: PathBuf,
+    relative_path: PathBuf,
 }
 
-impl SyncedDir for Slice {
-    fn get_file_paths(&self) -> Result<Vec<PathBuf>> {
-        self.globs.clone().into_iter()
-            .flat_map(|pattern| glob(&pattern).unwrap())
-            .map(|v| v.map_err(|_| Error::Generic("Failed to read glob pattern".into())))
-            .collect::<Result<Vec<_>>>()
-            .map(|paths| paths.clone().into_iter()
-                .unique()
-                .map(|p| self.root_path.join(p))
-                .filter(|path| path.is_file() && !is_ignored(&self.ignore_patterns, path, &self.root_path))
-                .collect())
-            .map_err(|_| Error::Generic("Failed to read glob pattern".into()))
+fn to_tracked_file(relative_path: &PathBuf, root_path: &PathBuf)-> TrackedFile {
+    TrackedFile {
+        absolute_path: root_path.join(&relative_path),
+        relative_path: relative_path.into(),
+    }
+}
+
+struct Dotr {
+    repo: Dir,
+    home: Dir,
+}
+
+impl Dotr {
+    fn new(repo: Dir, home: Dir) -> Self {
+        Self {
+            repo,
+            home,
+        }
     }
 
-    fn update_files(&self, source: &Slice) -> Result<()> {
-        let current_files = self.get_file_paths()?;
-        for file in current_files {
-            println!("Removing file {:?}", file);
-            remove_file(file)?;
+    fn sync(&self) -> Result<()> {
+        let repo_files = &self.repo.tracked_files;
+        for file in repo_files {
+            println!("Removing file {}", &file.absolute_path.display());
+            remove_file(&file.absolute_path).map_err(|_| Error::Generic("Failed to remove file".into()))?;
         }
-        let files_to_copy = source.get_file_paths()?;
-        for file in files_to_copy {
-            let relative_path = &file.strip_prefix(&source.root_path).unwrap();
-            let absolute_path = self.root_path.join(relative_path);
-            if let Err(_) = create_dir_all(absolute_path.parent().unwrap()) {
-                return Err(Error::Generic("Failed to create parent dir".into()));
+        let home_files = &self.home.tracked_files;
+        for file in home_files {
+            let destination = self.repo.root_path.join(&file.relative_path);
+            println!("Copying file {} to {}", file.absolute_path.display(), destination.display());
+            match copy(&file.absolute_path, destination) {
+                Ok(_) => Ok(()),
+                Err(_) => Err(Error::Generic("Failed to copy file".into())),
+            }?;
+        }
+        Ok(())
+    }
+
+    // fn force_install(&self) -> Result<()> {
+    //     let repo_files = &self.repo.tracked_files;
+    //     for file in repo_files {
+    //         let destination = self.home.root_path.join(&file.relative_path);
+    //         println!("Copying file {} to {}", file.absolute_path.display(), destination.display());
+    //         match copy(&file.absolute_path, destination) {
+    //             Ok(_) => Ok(()),
+    //             Err(_) => Err(Error::Generic("Failed to copy file".into())),
+    //         }?;
+    //     }
+    //     Ok(())
+    // }
+
+    fn install(&self) -> Result<()> {
+        let repo_files = &self.repo.tracked_files;
+        for file in repo_files {
+            let destination = self.home.root_path.join(&file.relative_path);
+            if !destination.exists() {
+                println!("Copying file {} to {}", file.absolute_path.display(), destination.display());
+                match copy(&file.absolute_path, destination) {
+                    Ok(_) => Ok(()),
+                    Err(_) => Err(Error::Generic("Failed to copy file".into())),
+                }?;
             }
-            println!("Copying {:#?} to {:#?}", file, absolute_path);
-            if let Err(_) = copy(file, absolute_path) {
-                return Err(Error::Generic("Failed to copy file".into()));
-            }
+            println!("{} already exists, skipping copy", file.relative_path.display());
         }
         Ok(())
     }
 }
 
-impl Slice {
-    fn new(root_path: PathBuf, globs: Vec<String>, ignore_patterns: Vec<String>) -> Self {
-        Self {root_path, globs, ignore_patterns}
-    }
+fn run_in_dir<T, F: FnOnce() -> T>(dir: &PathBuf, f: F) -> io::Result<T> {
+    let current_dir = env::current_dir()?;
+    env::set_current_dir(&dir)?;
+    let result = f();
+    env::set_current_dir(&current_dir)?;
+    Ok(result)
 }
 
-pub struct Dotr {
-    repo: Slice,
-    home: Slice
+fn get_tracked_files(root_path: &PathBuf, include_patterns: &Vec<String>, exclude_patterns: &Vec<String>) -> Result<Vec<TrackedFile>> {
+    println!("Getting tracked files for {}", root_path.display());
+    run_in_dir(&root_path, || {
+        let mut ignore_patterns = Vec::new();
+        for p in exclude_patterns {
+            ignore_patterns.push(Pattern::new(p).map_err(|_| Error::Generic("Bad pattern".into()))?);
+        }
+
+        let mut files = Vec::new();
+        for pattern in include_patterns {
+            let paths = glob(&pattern).map_err(|_| Error::Generic("Bad pattern".into()))?;
+            for path in paths {
+                let relative_path = path.map_err(|_| Error::Generic("Bad pattern".into()))?;
+                if ignore_patterns.iter().any(|p| p.matches(relative_path.to_str().unwrap())) {
+                    println!("Ignoring file {}", &root_path.join(&relative_path).display());
+                } else {
+                    println!("Added file {}", &root_path.join(&relative_path).display());
+                    files.push(to_tracked_file(&relative_path, &root_path));
+                }
+            }
+        }
+        Ok(files)
+    })?
 }
 
-impl Dotr {
-    fn new(repo: Slice, home: Slice) -> Result<Self> {
-        Ok(Self {repo, home})
-    }
-
-    fn sync(&self) -> Result<()> {
-        self.repo.update_files(&self.home)
-    }
-
-    fn install(&self) -> Result<()> {
-        self.home.update_files(&self.repo)
-    }
-}
-
-pub fn assert_repo_exists(dir: &PathBuf) -> Result<()> {
+fn assert_repo_exists(dir: &PathBuf) -> Result<()> {
     if let Err(_) = create_dir_all(&dir) {
         return Err(Error::Generic("Failed to create data directory".into()));
     };
@@ -108,58 +135,62 @@ pub fn assert_repo_exists(dir: &PathBuf) -> Result<()> {
     }
 }
 
-pub fn get_patterns_from_file(file_path: &PathBuf) -> Vec<String> {
+fn get_patterns_from_file(file_path: &PathBuf) -> Vec<String> {
     if let Ok(lines) = read_to_string(file_path) {
         return lines.lines().map(|l| l.to_string()).collect();
     };
     vec![]
 }
 
-pub fn get_repo_dir() -> Result<PathBuf> {
+fn get_repo_dir() -> Result<PathBuf> {
     let Some(project_dir) = ProjectDirs::from("dev", "thales-maciel", "dotr") else {
         return Err(Error::Generic("No valid path could be retrieved from system".into()))
     };
     Ok(project_dir.data_dir().to_owned())
 }
 
-pub fn get_ignore_file() -> PathBuf {
-    get_repo_dir().unwrap().join(".gitignore").into()
-}
-
 // todo: make this a get_or_create instead
-pub fn get_config_file() -> Result<PathBuf> {
+fn get_config_file() -> Result<PathBuf> {
     let Some(project_dir) = ProjectDirs::from("dev", "thales-maciel", "dotr") else {
         return Err(Error::Generic("No valid path could be retrieved from system".into()))
     };
     Ok(project_dir.config_dir().join("dotr.config"))
 }
 
-pub fn get_home_dir() -> Result<PathBuf> {
+fn get_home_dir() -> Result<PathBuf> {
     let Some(base_dir) = BaseDirs::new() else {
         return Err(Error::Generic("No valid path could be retrieved from system".into()))
     };
     Ok(base_dir.home_dir().to_owned())
 }
 
-fn main() -> Result<()> {
+fn build_dotr() -> Result<Dotr> {
     let home_dir = get_home_dir()?;
-
-    // Always run from home dir
-    env::set_current_dir(&home_dir)?;
-
     let repo_dir = get_repo_dir()?;
     assert_repo_exists(&repo_dir)?;
 
+    let include_file = get_config_file()?;
     let ignore_file = repo_dir.join(".gitignore");
+
+    let include_patterns = get_patterns_from_file(&include_file);
     let ignore_patterns = get_patterns_from_file(&ignore_file);
 
-    let config_file = get_config_file()?;
-    let config_patterns = get_patterns_from_file(&config_file);
+    let mut exclude_patterns = vec![".git/**".to_string(), ".gitignore".to_string()];
+    exclude_patterns.extend_from_slice(&ignore_patterns);
 
-    let repo = Slice::new(repo_dir, config_patterns.clone(), ignore_patterns.clone());
-    let home = Slice::new(home_dir, config_patterns.clone(), ignore_patterns.clone());
+    let tracked_files_in_repo = get_tracked_files(&repo_dir, &include_patterns, &exclude_patterns)?;
+    let repo = Dir { root_path: repo_dir, tracked_files: tracked_files_in_repo };
 
-    let dotr = Dotr::new(repo, home)?;
+    let tracked_files_in_home = get_tracked_files(&home_dir, &include_patterns, &exclude_patterns)?;
+    let home = Dir { root_path: home_dir, tracked_files: tracked_files_in_home };
+
+    let dotr = Dotr::new(repo, home);
+    Ok(dotr)
+}
+
+
+fn main() -> Result<()> {
+    let dotr = build_dotr()?;
 
     let cli = Cli::parse();
     match &cli.command {
@@ -172,7 +203,6 @@ fn main() -> Result<()> {
         Commands::Pwd => {
             println!("{}", get_repo_dir()?.display());
         }
-        _ => {}
     };
     Ok(())
 }
@@ -188,13 +218,11 @@ pub struct Cli {
 #[derive(Subcommand)]
 pub enum Commands {
     /// Adds paths to track
-    Add(AddArgs),
+    // Add(AddArgs),
     /// Updates the Dotr repository with all tracked files
     Sync,
     /// Prints the Dotr repository directory location
     Pwd,
-    /// Lists all tracked files
-    Ls,
     /// Places all tracked files into their destination
     Install,
 }
