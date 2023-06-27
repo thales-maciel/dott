75d9a2489a05803099fbe2c65c4778c018afbf59
Thales Maciel <thales@thalesmaciel.com>
2023-06-27 12:24:48 -0300

    basic refactor

--- src/lib.rs
+++ src/lib.rs
@@ -59,76 +59,50 @@ pub fn sync_dirs(
     }
 
     // go to from_dir
-    env::set_current_dir(&from_dir).map_err(DottError::IO)?;
-
-    // get all patterns from file
-    let patterns = read_to_string(pattern_file)
-        .map_err(DottError::IO)?
-        .lines()
-        .map(|l| l.to_string())
-        .collect::<Vec<String>>();
-
-    // get all matching files
-    let mut add_ops: Vec<Add> = Vec::new();
-    let mut overwrite_ops: Vec<Overwrite> = Vec::new();
-    let mut remove_ops: Vec<Remove> = Vec::new();
-
-    let mut files: Vec<PathBuf> = Vec::new();
-    for pattern in &patterns {
-        let paths = glob(pattern).map_err(|e| DottError::BadGlob(pattern.clone(), e))?;
-        for path in paths {
-            let path = path.map_err(|e| DottError::PathAccess(pattern.clone(), e))?;
-            if path.is_dir() {
-                continue;
-            }
-            let absolute_path = from_dir.join(&path);
-            if !files.iter().any(|f| f == &absolute_path) {
-                files.push(absolute_path.clone());
-                let target_path = to_dir.join(&path);
-                if target_path.exists() {
-                    overwrite_ops.push(Overwrite {
-                        from: absolute_path.clone(),
-                        to: target_path,
-                    })
-                } else {
-                    add_ops.push(Add {
-                        from: absolute_path,
-                        to: target_path,
-                    })
-                }
-            }
-        }
-    }
-
-    // go to to_dir
-    env::set_current_dir(&to_dir).map_err(DottError::IO)?;
-
-    // get all matching destination files
-    let mut files_to_delete: Vec<PathBuf> = Vec::new();
-    for pattern in &patterns {
-        let paths = glob(pattern).map_err(|e| DottError::BadGlob(pattern.clone(), e))?;
-        for path in paths {
-            let path = path.map_err(|e| DottError::PathAccess(pattern.clone(), e))?;
-            if path.is_dir() {
-                continue;
-            }
-            let absolute_path = to_dir.join(path);
-            // find out if path is going to be overwritten
-            if !overwrite_ops.iter().any(|o| o.to == absolute_path) {
-                continue;
-            }
-            if !files_to_delete.iter().any(|f| f == &absolute_path) {
-                files_to_delete.push(absolute_path.clone());
-                remove_ops.push(Remove(absolute_path));
-            }
-        }
-    }
+    let (add_ops, overwrite_ops, remove_ops) = compute_operations(from_dir, pattern_file, &to_dir)?;
 
     if add_ops.is_empty() && overwrite_ops.is_empty() && remove_ops.is_empty() {
         println!("No syncing necessary");
         return Ok(());
     }
 
+    print_operations(&add_ops, to_dir, &overwrite_ops, &remove_ops);
+
+    if raw.to_owned() {
+        return Ok(());
+    }
+
+    // ask the user to confirm
+    if Confirm::new()
+        .wait_for_newline(true)
+        .default(true)
+        .show_default(true)
+        .with_prompt("Do you want to continue?")
+        .interact_on(&Term::stdout())?
+    {
+        perform_operations(add_ops, overwrite_ops, remove_ops)?;
+    }
+
+    Ok(())
+}
+
+fn perform_operations(add_ops: Vec<Add>, overwrite_ops: Vec<Overwrite>, remove_ops: Vec<Remove>) -> Result<()> {
+    for add in add_ops.iter() {
+        create_dir_all(add.to.parent().unwrap())?;
+        copy(&add.from, &add.to)?;
+    }
+    for overwrite in overwrite_ops.iter() {
+        create_dir_all(overwrite.to.parent().unwrap())?;
+        copy(&overwrite.from, &overwrite.to)?;
+    }
+    for remove in remove_ops.iter() {
+        remove_file(&remove.0)?;
+    }
+    println!("{}", style("Sync completed successfully").bold());
+    Ok(())
+}
+
+fn print_operations(add_ops: &Vec<Add>, to_dir: std::borrow::Cow<std::path::Path>, overwrite_ops: &Vec<Overwrite>, remove_ops: &Vec<Remove>) {
     if !add_ops.is_empty() {
         println!("The following files will be added to {}", to_dir.display());
         for add in add_ops.iter() {
@@ -158,36 +132,67 @@ pub fn sync_dirs(
         }
         println!();
     }
+}
 
-    if raw.to_owned() {
-        return Ok(());
+fn compute_operations(
+    from_dir: std::borrow::Cow<std::path::Path>,
+    pattern_file: &PathBuf,
+    to_dir: &std::borrow::Cow<std::path::Path>
+) -> Result<(Vec<Add>, Vec<Overwrite>, Vec<Remove>)> {
+    env::set_current_dir(&from_dir).map_err(DottError::IO)?;
+    let patterns = read_to_string(pattern_file)
+        .map_err(DottError::IO)?
+        .lines()
+        .map(|l| l.to_string())
+        .collect::<Vec<String>>();
+    let mut add_ops: Vec<Add> = Vec::new();
+    let mut overwrite_ops: Vec<Overwrite> = Vec::new();
+    let mut remove_ops: Vec<Remove> = Vec::new();
+    let mut files: Vec<PathBuf> = Vec::new();
+    for pattern in &patterns {
+        let paths = glob(pattern).map_err(|e| DottError::BadGlob(pattern.clone(), e))?;
+        for path in paths {
+            let path = path.map_err(|e| DottError::PathAccess(pattern.clone(), e))?;
+            if path.is_dir() {
+                continue;
+            }
+            let absolute_path = from_dir.join(&path);
+            if !files.iter().any(|f| f == &absolute_path) {
+                files.push(absolute_path.clone());
+                let target_path = to_dir.join(&path);
+                if target_path.exists() {
+                    overwrite_ops.push(Overwrite {
+                        from: absolute_path.clone(),
+                        to: target_path,
+                    })
+                } else {
+                    add_ops.push(Add {
+                        from: absolute_path,
+                        to: target_path,
+                    })
+                }
+            }
+        }
     }
-
-    // ask the user to confirm
-    if Confirm::new()
-        .wait_for_newline(true)
-        .default(true)
-        .show_default(true)
-        .with_prompt("Do you want to continue?")
-        .interact_on(&Term::stdout())?
-    {
-        // add all files
-        for add in add_ops.iter() {
-            create_dir_all(add.to.parent().unwrap())?;
-            copy(&add.from, &add.to)?;
+    env::set_current_dir(to_dir).map_err(DottError::IO)?;
+    let mut files_to_delete: Vec<PathBuf> = Vec::new();
+    for pattern in &patterns {
+        let paths = glob(pattern).map_err(|e| DottError::BadGlob(pattern.clone(), e))?;
+        for path in paths {
+            let path = path.map_err(|e| DottError::PathAccess(pattern.clone(), e))?;
+            if path.is_dir() {
+                continue;
+            }
+            let absolute_path = to_dir.join(path);
+            // find out if path is going to be overwritten
+            if !overwrite_ops.iter().any(|o| o.to == absolute_path) {
+                continue;
+            }
+            if !files_to_delete.iter().any(|f| f == &absolute_path) {
+                files_to_delete.push(absolute_path.clone());
+                remove_ops.push(Remove(absolute_path));
+            }
         }
-        // overwrite all files
-        for overwrite in overwrite_ops.iter() {
-            create_dir_all(overwrite.to.parent().unwrap())?;
-            copy(&overwrite.from, &overwrite.to)?;
-        }
-        // remove all files
-        for remove in remove_ops.iter() {
-            remove_file(&remove.0)?;
-        }
-
-        println!("{}", style("Sync completed successfully").bold());
     }
-
-    Ok(())
+    Ok((add_ops, overwrite_ops, remove_ops))
 }
